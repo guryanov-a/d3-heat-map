09aec594122066a429151bad1e7d0d3286d9629d
Artyom Guryanov <guryanov.artyom@gmail.com>
2019-05-25 22:55:29 +0600

    increase heat map padding to fit axes

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const PLOT_DATA_PATH: string = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
 export const CHART_WIDTH: number = 1600;
 export const CHART_HEIGHT: number = 600;
-export const CHART_PADDING: number = 60;
+export const CHART_PADDING: number = 70;
