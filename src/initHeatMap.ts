29c2fdd932161ce06f436ec369b1ead5eac22fa7
Artyom Guryanov <artyom.guryanov@csssr.com>
2019-08-06 02:04:23 +0600

    initHeatMap: add basic tooltip hover code

--- src/initHeatMap.ts
+++ src/initHeatMap.ts
@@ -15,4 +15,27 @@ export async function initHeatMap(): Promise<void> {
 
   const dataset = parseServerHeatMapData(monthlyVariance);
   createHeatMap(dataset);
+  
+  const heatMapEl = document.querySelector('#heat-map');
+  const tooltip = document.querySelector('#tooltip');
+
+  heatMapEl.addEventListener("mouseover", (event): void => {
+    const eventTarget = event.target as SVGElement | HTMLElement;
+    if (!eventTarget.classList.contains('cell')) return;
+    const targetCoords = eventTarget.getBoundingClientRect();
+    console.log(targetCoords);
+
+    tooltip.style.top = `${targetCoords.top}px`;
+    tooltip.style.left = `${targetCoords.left + targetCoords.width}px`;
+    tooltip.classList.add('tooltip_visibility_visible');
+  });
+
+  heatMapEl.addEventListener("mouseout", (event): void => {
+    const eventTarget = event.target as SVGElement | HTMLElement;
+    if (!eventTarget.classList.contains('cell')) return;
+
+    eventTarget.style.top = null;
+    eventTarget.style.left = null;
+    tooltip.classList.remove('tooltip_visibility_visible');
+  });
 }
